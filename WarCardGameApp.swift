0028d0c6bdf636213642b53fdd68247d2fdbd2ed
Mohammed D <87583993+m0diallo@users.noreply.github.com>
2023-11-14 09:36:41 -0500

    Upload WarCardGameApp.swift

---
+++ WarCardGameApp.swift
@@ -0,0 +1,17 @@
+//
+//  War_Card_GameApp.swift
+//  War Card Game
+//
+//  Created by Mohammed Diallo on 2023-09-15.
+//
+
+import SwiftUI
+
+@main
+struct War_Card_GameApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
